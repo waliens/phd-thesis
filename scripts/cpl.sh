68a54bc146750e3227cc7b6ed6e04b798c23d720
waliens <romainmormont@hotmail.com>
2022-05-24 10:00:30 +0200

    new dockerfile version

--- scripts/cpl.sh
+++ scripts/cpl.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --rm --volume="$PWD":"/data" -t rmormont/thesis-tex-compile:1.0 python3 /scripts/compile.py "$@"
+docker run --rm --volume="$PWD":"/data" -t rmormont/thesis-tex-compile:1.1 python3 /scripts/compile.py "$@"
