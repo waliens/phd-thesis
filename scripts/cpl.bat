fb85f0c13471682c7337a25655ee87387fd548e6
waliens <romainmormont@hotmail.com>
2022-06-10 14:16:46 +0200

    continue section results in chap 6

--- scripts/cpl.bat
+++ scripts/cpl.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-docker run --rm --volume="%cd%":"/data" -t rmormont/thesis-tex-compile:1.0 python3 /scripts/compile.py %* 
+docker run --rm --volume="%cd%":"/data" -t rmormont/thesis-tex-compile:1.1 python3 /scripts/compile.py %* 
