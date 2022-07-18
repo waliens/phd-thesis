adfba9d823cb0abb1753af20bc8bff174ec1f55c
waliens <romainmormont@hotmail.com>
2022-07-18 18:06:53 +0200

    latexdiff and expend image v1.4

--- scripts/cpl.sh
+++ scripts/cpl.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --rm --volume="$PWD":"/data" -t rmormont/thesis-tex-compile:1.3 python3 /scripts/compile.py "$@"
+docker run --rm --volume="$PWD":"/data" -t rmormont/thesis-tex-compile:1.4 python3 /scripts/compile.py "$@"
